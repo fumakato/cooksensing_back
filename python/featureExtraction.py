c46758acdae65a60f7a94c5c1253209b300cf5a8
fumakato <k20035kk@aitech.ac.jp>
2025-04-07 16:43:56 +0900

    nn

--- python/featureExtraction.py
+++ python/featureExtraction.py
@@ -33,10 +33,6 @@ def feature_extraction(url):
                 'code': 'CSV_READ_ERROR'
             }), 400
 
-        # # fname = "kakumu"
-        # # wAcc = pd.read_csv(f"cut/{fname}_acc.csv", encoding = 'utf-8')
-        # wAcc = pd.read_csv(f"cut/fuma_evaluation/{fname}-acc.csv", encoding = 'utf-8')
-
          # 必要な列があるかチェック
         for col in ['x', 'y', 'z', 'time']:
             if col not in wAcc.columns:
@@ -62,7 +58,7 @@ def feature_extraction(url):
             timeDeff = -3000
             thFlg = True
             for i in range(len(data)):
-                if (wAcc["time"][i] - timeDeff)/1000 > 0.2: timeFlg = True
+                if (wAcc["time"][i] - timeDeff)/1000 > 0.3: timeFlg = True
                 if data[i] > threshold:
                     if timeFlg and thFlg:
                         timeFlg = False
@@ -77,77 +73,30 @@ def feature_extraction(url):
                     thFlg = False
                 else: thFlg = True
 
-            if len(cutListData) < 5:
-                return jsonify({
-                    'error': '十分なピークデータが検出できませんでした。',
-                    'code': 'INSUFFICIENT_PEAKS'
-                }), 422
 
-            #ここから新しく入れたところ 
-            # 一番目と二番目に大きいデータを取得
-            top_2 = heapq.nlargest(2, cutListData)
-            # 一番目と二番目に大きいデータのインデックスを取得
-            top_1_index = cutListData.index(top_2[0])
-            top_2_index = cutListData.index(top_2[1])
-            # 切ってない部分を削除
-            cutList = cutList[top_1_index+1:top_2_index]
-            cutListTime = cutListTime[top_1_index+1:top_2_index]
-            cutListData = cutListData[top_1_index+1:top_2_index]
-            # 切ってない部分を削除
-            # cutList = cutList[top_1_index+1:]
-            # cutListTime = cutListTime[top_1_index+1:]
-            # cutListData = cutListData[top_1_index+1:]
-            if j==0:
+            if j==0:  
+                if len(cutListData) < 7:
+                    return jsonify({
+                        'error': '十分なピークデータが検出できませんでした。',
+                        'code': 'INSUFFICIENT_PEAKS'
+                    }), 422
+                #ここから新しく入れたところ 
+                # 一番目と二番目に大きいデータを取得
+                top_2 = heapq.nlargest(2, cutListData)
+                # 一番目と二番目に大きいデータのインデックスを取得
+                top_1_index = cutListData.index(top_2[0])
+                top_2_index = cutListData.index(top_2[1])
+                # 切ってない部分を削除
+                cutList = cutList[top_1_index+1:top_2_index]
+                cutListTime = cutListTime[top_1_index+1:top_2_index]
+                cutListData = cutListData[top_1_index+1:top_2_index]
+
                 # 上から5つの大きいデータを取得
                 average = sum(heapq.nlargest(5, cutListData)) / 5
                 threshold = average * 0.8
                 print("cut:"+str(len(cutList)))
-                # fuseya 0.77
-                # ao 0.83
-                # 
-
-        # cutNum = 0
-        # tmp = 0
-        # while cutNum<35: # 極度に回数が少ないときは閾値を下げる
-        #     threshold = 13.5 - ( tmp * 0.55 )
-        #     timeFlg = True
-        #     timeDeff = -3000
-        #     thFlg = True
-        #     for i in range(len(data)):
-        #         if (wAcc["time"][i] - timeDeff)/1000 > 0.2: timeFlg = True
-        #         if data[i] > threshold:
-        #             if timeFlg and thFlg:
-        #                 timeFlg = False
-        #                 timeDeff = wAcc["time"][i]
-        #                 cutList.append([wAcc["time"][i], data[i]])
-        #                 cutListTime.append(wAcc["time"][i])
-        #                 cutListData.append(data[i])
-        #             elif cutList[len(cutList)-1][1] < data[i]: 
-        #                 cutList[len(cutList)-1] = [wAcc["time"][i], data[i]]
-        #                 cutListTime[len(cutList)-1] = wAcc["time"][i]
-        #                 cutListData[len(cutList)-1] = data[i]
-        #             thFlg = False
-        #         else: thFlg = True
-        #     # 一番目と二番目に大きいデータを取得
-        #     top_2 = heapq.nlargest(2, cutListData)
-        #     # 一番目と二番目に大きいデータのインデックスを取得
-        #     top_1_index = cutListData.index(top_2[0])
-        #     top_2_index = cutListData.index(top_2[1])
-        #     # 切ってない部分を削除
-        #     # cutList = cutList[top_1_index+1:top_2_index]
-        #     # cutListTime = cutListTime[top_1_index+1:top_2_index]
-        #     # cutListData = cutListData[top_1_index+1:top_2_index]
-        #     cutList = cutList[top_1_index+1:]
-        #     cutListTime = cutListTime[top_1_index+1:]
-        #     cutListData = cutListData[top_1_index+1:]
-        #     cutNum = len(cutList)
-        #     print(tmp, threshold, cutNum)
-        #     tmp = tmp + 1
-            
 
         print("cut:"+str(len(cutList)))
-        # print(cutList)
-
 
         tempo = -1
         tempoRap = []
